102dd3e32cdfa7a2dfb3b93ccbd80b92fa060ee7
imhotkp <singhkaushlendra1990@gmail.com>
2025-01-29 23:41:05 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// // Handle form submission
+// document.getElementById("contactForm").addEventListener("submit", function (event) {
+//     event.preventDefault();  // Prevent form from submitting to the server
+
+//     // Show success message
+//     document.getElementById("successMessage").classList.remove("d-none");
+
+//     // Optionally clear form fields after submission
+//     document.getElementById("contactForm").reset();
+
+//     // You can use Ajax to submit the form data without reloading the page
+//     // Fetch or XMLHttpRequest can be used here if needed
+// });
